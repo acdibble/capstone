7acb5df9d27e72bdda22d804dcdc6fa3f87350f6
Andrew Dibble <andrew@dibble.codes>
2021-04-21 10:04:19 +0200

    Model training take 2

--- model/src/database.ts
+++ model/src/database.ts
@@ -1,15 +1,10 @@
-import { EventEmitter } from 'events';
 import sqlite3 from 'sqlite3';
 import { getAsset } from './utils.js';
-
-export const sentiments = ['positive', 'neutral', 'negative'] as const;
-
-export type Sentiment = typeof sentiments[number]
 
 interface Tweet {
   textID: string;
   text: string;
-  sentiment: Sentiment;
+  sentiment: 'positive' | 'neutral' | 'negative';
 }
 
 export default class Database {
@@ -41,52 +36,13 @@ export default class Database {
     return this.runAsync`INSERT INTO tweets VALUES (${textID}, ${text}, ${sentiment})`;
   }
 
-  async query<T>(parts: TemplateStringsArray, ...args: unknown[]): Promise<T> {
-    const sql = parts.join('?');
+  query<T>(parts: TemplateStringsArray, ...args: unknown[]): Promise<T[]> {
     return new Promise((resolve, reject) => {
-      this.db.get(sql, args, (error, result) => (error ? reject(error) : resolve(result)));
+      this.db.all(parts.join('?'), args, (error, result) => (error ? reject(error) : resolve(result)));
     });
   }
 
-  async* [Symbol.asyncIterator]({ limit, random }: Partial<{limit: number; random: boolean}>): AsyncGenerator<Tweet> {
-    const emitter = new EventEmitter();
-
-    let sql = 'SELECT trim(lower(text)) as text, sentiment FROM tweets';
-    if (random) sql += ' ORDER BY RANDOM()';
-    if (limit) sql += ` LIMIT ${limit}`;
-    this.db.each(
-      sql,
-      (error, result) => {
-        if (error) {
-          emitter.emit('error', error);
-        } else {
-          emitter.emit('tweet', result);
-        }
-      },
-      (error) => {
-        if (error) {
-          emitter.emit('error', error);
-        } else {
-          emitter.emit('end', null);
-        }
-      },
-    );
-
-    let tweet: Tweet | null;
-    const endPromise = new Promise<null>((resolve) => emitter.once('end', resolve));
-    const errorPromise = new Promise<never>((_, reject) => emitter.once('error', reject));
-    do {
-      tweet = await Promise.race([
-        new Promise<Tweet>((resolve) => emitter.once('tweet', resolve)),
-        errorPromise,
-        endPromise,
-      ]);
-
-      if (tweet) yield tweet;
-    } while (tweet !== null);
-  }
-
-  random(limit = 100): AsyncGenerator<Tweet> {
-    return this[Symbol.asyncIterator]({ limit, random: true });
+  all(): Promise<Tweet[]> {
+    return this.query`SELECT trim(lower(text)) as text, sentiment FROM tweets ORDER BY RANDOM()`;
   }
 }
--- model/src/train.ts
+++ model/src/train.ts
@@ -1,6 +1,8 @@
+import url from 'url';
 import use from '@tensorflow-models/universal-sentence-encoder';
 import tf from '@tensorflow/tfjs-node-gpu';
-import Database, { Sentiment, sentiments } from './database.js';
+import Database from './database.js';
+import { getAsset } from './utils.js';
 
 const getModel = (): tf.Sequential => {
   const model = tf.sequential();
@@ -33,68 +35,69 @@ const getModel = (): tf.Sequential => {
 
 interface DataSet {
   texts: string[];
-  intents: Record<Sentiment, [number, number][]>;
   textSentiments: string[];
 }
 
-const getDataSet = async (
-  database: Database,
-  limit?: number,
-): Promise<DataSet> => {
-  const total = limit ?? (await database.query<{total: number}>`SELECT COUNT(1) as total FROM tweets`).total;
+interface DataSetWithIntents extends DataSet {
+  intents: [number, number][];
+}
+
+async function getDataSet(database: Database, returnIntents: false): Promise<DataSet>
+async function getDataSet(database: Database, returnIntents: true): Promise<DataSetWithIntents>
+async function getDataSet(database: Database, returnIntents: boolean): Promise<DataSet | DataSetWithIntents> {
+  const [{ total }] = await database.query<{ total: number }>`SELECT COUNT(1) as total FROM tweets`;
   const texts = Array(total) as string[];
-  const intents: Record<Sentiment, [number, number][]> = {
-    negative: Array(total) as [number, number][],
-    neutral: Array(total) as [number, number][],
-    positive: Array(total) as [number, number][],
-  };
+  const intents = Array(returnIntents ? 0 : total) as [number, number][];
   const textSentiments = Array(total) as string[];
-  let count = 0;
-  for await (const { text, sentiment } of database.random(limit)) {
-    texts[count] = text;
-    textSentiments[count] = sentiment;
-    for (const s of sentiments) {
-      intents[s][count] = [sentiment !== s ? 1 : 0, sentiment === s ? 1 : 0];
-    }
-    count += 1;
+  const tweets = await database.all();
+  for (let i = 0; i < tweets.length; i++) {
+    const { text, sentiment } = tweets[i];
+    texts[i] = text.replace(/[^a-z ]/gi, '');
+    textSentiments[i] = sentiment;
+    if (returnIntents) intents[i] = [sentiment !== 'negative' ? 1 : 0, sentiment === 'negative' ? 1 : 0];
   }
-  return { texts, intents, textSentiments };
-};
+  return returnIntents ? { texts, intents, textSentiments } : { texts, textSentiments };
+}
 
-const is = ([, match]: [number, number], label: string): Record<string, boolean> => ({ [label]: match > 0.9 });
-
-const evaluate = async (models: tf.Sequential[], dataSet: DataSet, testData: tf.Tensor2D): Promise<void> => {
+const evaluate = async (model: tf.Sequential, dataSet: DataSet, testData: tf.Tensor2D): Promise<void> => {
   const { texts, textSentiments } = dataSet;
-  const results = models.map((model) => model.predict(testData));
-  // @ts-expect-error test
-  const numberResults = await Promise.all(results.map((result) => result.array() as Promise<[number, number][]>));
-  // eslint-disable-next-line @typescript-eslint/prefer-for-of
+  // @ts-expect-error another bad typing
+  const results = await model.predict(testData).array() as [number, number][];
   for (let i = 0; i < texts.length; i++) {
     console.log('%o', {
       text: texts[i],
       sentiment: textSentiments[i],
-      ...numberResults.reduce((acc, result, j) => ({ ...acc, ...is(result[i], sentiments[j]) }), {}),
+      result: results[i],
+      success: textSentiments[i] === 'negative' ? results[i][1] > 0.9 : results[i][1] < 0.9,
     });
   }
 };
 
 const main = async (): Promise<void> => {
-  const database = await Database.for('train');
-  const [encoder, training, testing] = await Promise.all([
-    use.load(),
-    getDataSet(database, 10000),
-    getDataSet(database, 100),
-  ]);
+  const trainingDb = await Database.for('train');
+  const encoder = await use.load();
+  let training: DataSetWithIntents | null = await getDataSet(trainingDb, true);
   console.log('embedding training data');
-  const [trainingData, testData] = await Promise.all([
-    encoder.embed(training.texts),
-    encoder.embed(testing.texts),
-  ]);
-  const outputData = sentiments.map((sentiment) => tf.tensor2d(training.intents[sentiment]));
-  const models = sentiments.map(() => getModel());
+  let trainingData: tf.Tensor2D | null = null;
+  for (let i = 0; i < training.texts.length; i += 100) {
+    const tempData = await encoder.embed(training.texts.slice(i, i + 100));
+    trainingData = trainingData?.concat(tempData) ?? tempData;
+  }
+  if (trainingData === null) throw new Error();
+  const outputData = tf.tensor2d(training.intents);
+  const model = getModel();
   console.log('fitting model');
-  await Promise.all(models.map((model, i) => model.fit(trainingData, outputData[i], { epochs: 200 })));
-  await evaluate(models, testing, testData);
+  await model.fit(trainingData, outputData, { epochs: 300 });
+  console.log('saving model');
+  const modelpath = url.pathToFileURL(getAsset('trained-model'));
+  await model.save(modelpath.toString());
+  training = null;
+  trainingData.dispose();
+  outputData.dispose();
+  const testingDb = await Database.for('test');
+  const testing = await getDataSet(testingDb, false);
+  const testData = await encoder.embed(testing.texts);
+  await evaluate(model, testing, testData);
 };
 
 void main();
