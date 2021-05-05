c7a98e76778511ae5c67aa013047600ff061fb31
Andrew Dibble <andrew@dibble.codes>
2021-05-05 21:53:32 +0200

    Filter graphql response

--- extension/src/script.ts
+++ extension/src/script.ts
@@ -2,6 +2,57 @@
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const oldOpen = window.XMLHttpRequest.prototype.open;
 
+interface TraversalObject {
+  tweets: Record<string, Twitter.Tweet>;
+  tweetPaths: Record<string, (string | number)[]>;
+}
+
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable no-param-reassign */
+/* eslint-disable guard-for-in */
+function traverse(obj: Record<string, any> | Record<string, any>[]): TraversalObject
+function traverse(
+  obj: Record<string, any> | Record<string, any>[],
+  paths: (string | number)[],
+  ret: TraversalObject,
+): undefined
+function traverse(
+  obj: Record<string, any> | Record<string, any>[],
+  paths: (string | number)[] = [],
+  { tweets, tweetPaths }: TraversalObject = { tweets: {}, tweetPaths: {} },
+): TraversalObject | undefined {
+  for (const key in obj) {
+    if (key === 'tweet') {
+      const tweet = obj[key as keyof typeof obj].legacy as Twitter.Tweet;
+      tweets[tweet.id_str] = tweet;
+      tweetPaths[tweet.id_str] = [...paths, 'tweet'];
+      return;
+    }
+    paths.push(key);
+    const value = obj[key as keyof typeof obj];
+    if (typeof value === 'object' && value !== null && !Array.isArray(value)) {
+      traverse(value as Record<string, unknown>, paths, { tweetPaths, tweets });
+    } else if (Array.isArray(value)) {
+      for (let i = 0; i < value.length; i++) {
+        paths.push(i);
+        traverse(value[i], paths, { tweetPaths, tweets });
+        paths.pop();
+      }
+    }
+    paths.pop();
+  }
+
+  // eslint-disable-next-line consistent-return
+  return { tweetPaths, tweets };
+}
+/* eslint-enable @typescript-eslint/no-unsafe-member-access */
+/* eslint-enable no-param-reassign */
+/* eslint-enable @typescript-eslint/no-unsafe-assignment */
+/* eslint-enable @typescript-eslint/no-explicit-any */
+/* eslint-enable guard-for-in */
+
 interface IncomingMessage {
   id: number;
   classifications: Analyzer.Result[];
@@ -32,6 +83,7 @@ const filterTweets = async (tweetsObject: Record<string, Twitter.Tweet> | undefi
   if (!tweetsObject) return false;
   const tweetIds = Object.keys(tweetsObject);
   if (tweetIds.length === 0) return false;
+  console.time('classification');
   const tweets: Analyzer.Input[] = tweetIds.map((id) => {
     const {
       full_text: text,
@@ -57,6 +109,42 @@ const filterTweets = async (tweetsObject: Record<string, Twitter.Tweet> | undefi
 };
 /* eslint-enable no-param-reassign */
 
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const filterGraphQLResponse = async (responseBody: Record<string, any>): Promise<boolean> => {
+  const instructions = responseBody.data?.user?.result?.timeline?.timeline?.instructions;
+  if (!instructions) return false;
+  console.log('instructions:', instructions);
+  const { tweets, tweetPaths } = traverse(instructions);
+  const filtered = await filterTweets(tweets);
+  console.log(filtered);
+  if (!filtered) return false;
+  for (const [tweetId, parts] of Object.entries(tweetPaths)) {
+    // eslint-disable-next-line no-continue
+    if (tweetId in tweets) continue;
+    console.log('tweetId', tweetId);
+    const path = [];
+    let numberFound = false;
+    for (let i = parts.length - 1; i >= 0; i--) {
+      if (!numberFound && typeof parts[i] === 'number') numberFound = true;
+      if (numberFound) path.push(parts[i]);
+    }
+    console.log('path', path);
+
+    let obj = instructions;
+    while (path.length > 1) {
+      obj = obj[path.pop()!];
+    }
+    delete obj[path[0]];
+  }
+  console.log('instructions:', instructions);
+  return true;
+};
+/* eslint-enable @typescript-eslint/no-unsafe-member-access */
+/* eslint-enable @typescript-eslint/no-unsafe-assignment */
+/* eslint-enable @typescript-eslint/no-explicit-any */
+
 interface CreateCallbackOpts {
   callback?: (this: XMLHttpRequest) => void;
 }
@@ -67,7 +155,16 @@ const createCallback = (opts: CreateCallbackOpts) => async function (this: XMLHt
     if (status >= 200 && status < 300) {
       const responseBody = JSON.parse(this.responseText) as Twitter.Response;
 
-      if ((await filterTweets(responseBody?.globalObjects?.tweets))) {
+      let filtered: boolean;
+
+      if (/graphql\/[\w\d]+\/UserTweets/.test(this.responseURL)) {
+        console.log(responseBody);
+        filtered = await filterGraphQLResponse(responseBody);
+      } else {
+        filtered = await filterTweets(responseBody?.globalObjects?.tweets);
+      }
+      if (filtered) {
+        console.timeEnd('classification');
         Object.defineProperty(this, 'responseText', {
           get() {
             return JSON.stringify(responseBody);
